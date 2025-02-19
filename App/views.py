691716a8ccebae04bf2071b10772b71588a4b8b7
rr <rr@gmail.com>
2025-02-19 18:31:12 +0530

    first commit

--- App/views.py
+++ App/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Department, Employee, Role
+from .models import Department, Role
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView
 from django.contrib.auth.models import Permission
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from .forms import EmployeeForm
 
 
 def homepage(request):
@@ -54,67 +53,6 @@ class DepartmentDeleteView(DeleteView):
     template_name = 'department_delete.html'
     success_url = reverse_lazy('department_list')
 
-
-@login_required
-def assign_permission_to_user(request):
-    try:
-        employee_user = get_object_or_404(Employee, username="user1")
-        permission = Permission.objects.get(codename="can_add_employee")
-        employee_user.user_permissions.add(permission)
-
-        employee_user2 = get_object_or_404(Employee, username="user2")
-        permission2 = Permission.objects.get(codename="can_update_employee")
-        employee_user2.user_permissions.add(permission2)
-
-        return HttpResponse("Permissions added successfully.")
-    except Exception as e:
-        return HttpResponse(f"Error while assigning permissions: {e}")
-
-
-def EmployeeCreateView(request):
-    if request.method == 'POST':
-        employee_form = EmployeeForm(request.POST)
-        if employee_form.is_valid():
-            employee = employee_form.save(commit=False)
-            employee.username = employee_form.cleaned_data['f_name1']  
-            employee.set_password(employee_form.cleaned_data['password1'])  
-            employee.save()
-            return redirect('employee_list')
-    
-    else:
-        employee_form = EmployeeForm()
-    
-    return render(request,'employee_create_form.html',{'employee_form':employee_form})
-
-
-class EmployeeListView(ListView):
-    model=Employee
-    template_name='employee_list.html'
-    context_object_name='employees'
-
-    def get_queryset(self):
-        query = self.request.GET.get('q')
-        if query:
-            return Employee.objects.filter(f_name1__icontains=query) 
-        return Employee.objects.all()
-
-class EmployeeDetailView(DetailView):
-    model = Employee
-    template_name = 'employee_detail.html'
-    context_object_name = 'employee'
-
-
-class EmployeeUpdateView(UpdateView):
-    model = Employee
-    template_name = 'employee_update.html'
-    fields = ['f_name1', 'l_name1', 'email1', 'phone1', 'address1']
-    success_url = reverse_lazy('employee_list')
-
-
-class EmployeeDeleteView(DeleteView):
-    model = Employee
-    template_name = 'employee_delete.html'
-    success_url = reverse_lazy('employee_list')
 
 class RoleListView(ListView):
     model = Role
