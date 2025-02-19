f30d4a4b21dbce57eaea0b7cb7aa959927c26e82
rr <rr@gmail.com>
2025-02-19 18:20:42 +0530

    first commit

--- App/models.py
+++ App/models.py
@@ -23,23 +23,6 @@ class Role(models.Model):
         return self.r_name
 
 
-class Employee(User):
-    employee_id = models.AutoField(primary_key=True)
-    f_name = models.CharField(max_length=100)
-    username=models.CharField(max_length=100, unique=True)
-    password=models.CharField(max_length=100)
-    l_name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100, unique=True)
-    mobile = models.CharField(max_length=100)
-    department = models.ForeignKey('Department', on_delete=models.SET_NULL, null=True, blank=True)
-    role = models.ForeignKey('Role', on_delete=models.SET_NULL, null=True, blank=True)
-    reporting_manager = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
-    date_of_joining = models.DateField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f"{self.f_name} {self.l_name}"
 
 
 
--- App/urls.py
+++ App/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import homepage,EmployeeCreateView,DepartmentListView, DepartmentCreateView, DepartmentDetailView, DepartmentUpdateView, DepartmentDeleteView, EmployeeListView, EmployeeDetailView, EmployeeUpdateView, EmployeeDeleteView,RoleListView,RoleCreateView,RoleDeleteView,RoleDetailView,RoleUpdateView
+from .views import homepage,DepartmentListView, DepartmentCreateView, DepartmentDetailView, DepartmentUpdateView, DepartmentDeleteView,RoleListView,RoleCreateView,RoleDeleteView,RoleDetailView,RoleUpdateView
 
 urlpatterns = [
     path('home/', homepage, name='home'),
@@ -11,11 +11,6 @@ urlpatterns = [
     path('department_detail/<int:pk>/',DepartmentDetailView.as_view() , name='department_detail'),
     path('update/<int:pk>',DepartmentUpdateView.as_view() , name='department_update'),
     path('delete/<int:pk>', DepartmentDeleteView.as_view(), name='department_delete'),
-    path('employee_list/', EmployeeListView.as_view(), name='employee_list'),
-    path('employee_create/', EmployeeCreateView, name='employee_create_form'),
-    path('employee_detail/<int:pk>/', EmployeeDetailView.as_view(), name='employee_detail'),
-    path('employee_update/<int:pk>/', EmployeeUpdateView.as_view(), name='employee_update'),
-    path('employee_delete/<int:pk>/', EmployeeDeleteView.as_view(), name='employee_delete'),
     path('role_list/',RoleListView.as_view(),name="role_list"),
     path('role_create/',RoleCreateView.as_view(),name="role_create_form"),
     path('role_detail/<int:pk>/',RoleDetailView.as_view(),name="role_detail"),
